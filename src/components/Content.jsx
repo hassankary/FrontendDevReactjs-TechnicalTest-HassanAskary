a04fd243c1cf810558d7145af6b51ac4bde8edd0
Hassan Kaeru <hassanaskary29@gmail.com>
2024-08-30 03:14:30 +0700

    update style

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -12,21 +12,8 @@ export const Content = ({ dataResto }) => {
   const [price, setPrice] = useState("");
 
   useEffect(() => {
-    console.log("dataResto =>", dataResto);
     setData(dataResto);
   }, [dataResto]);
-
-  useEffect(() => {
-    console.log("category parent =>", category);
-  }, [category]);
-
-  useEffect(() => {
-    console.log("price=>", price);
-  }, [price]);
-
-  useEffect(() => {
-    console.log("data ==>", data);
-  }, [data]);
 
   const categoryHandler = (msg) => {
     setCategory(msg);
@@ -118,18 +105,20 @@ export const Content = ({ dataResto }) => {
   };
 
   return (
-    <div className="min-h-screen flex flex-col justify-center">
-      <div className="py-6 sm:py-12 px-[5%] md:px-[10%] space-y-3">
-        <h1 className="font-bold text-3xl ">Resturants</h1>
-        <p className="">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+    <div className="min-h-screen flex flex-col">
+      <div className="py-6 sm:py-12 p-[5%] md:px-[10%]">
+        <div className="max-w-6xl flex flex-col mx-auto space-y-3">
+          <h1 className="font-bold text-3xl ">Resturants</h1>
+          <p className="">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
       </div>
       <Navbar
         sendCategory={categoryHandler}
@@ -137,92 +126,94 @@ export const Content = ({ dataResto }) => {
         sendPrice={priceHandler}
       />
       <div className="h-full flex flex-col flex-grow py-6 sm:py-12 px-[5%] md:px-[10%]">
-        {!data ? (
-          <div className="w-full h-full flex-grow flex justify-center items-center animate-pulse">
-            <div>Loading...</div>
-          </div>
-        ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-            {data?.slice(more.a, more.b).map((d, i) => {
-              const imageUrl = `${d.squareImgUrl}`;
-              return (
-                <div
-                  key={i}
-                  className="flex flex-col text-black justify-between rounded-xl shadow-xl"
-                >
-                  <div>
-                    <div className="h-[150px]">
-                      <img
-                        src={imageUrl}
-                        alt={d.name}
-                        className="object-cover w-full h-full"
-                      />
-                    </div>
-                    <div className="p-2 text-sm">
-                      <h1 className="font-semibold text-base">{d.name}</h1>
-                      <h1>{d.parentGeoName}</h1>
-                      <Rate
-                        value={d.averageRating}
-                        allowHalf
-                        disabled
-                        style={{ color: "#172554" }}
-                        className="py-1.5 text-[16px]"
-                      />
-                      <div className="flex justify-between text-xs uppercase">
-                        <div className="flex space-x-1">
-                          <h1 className="">
-                            {d.establishmentTypeAndCuisineTags[0]} •
+        <div className="max-w-6xl h-full w-full flex flex-col flex-grow mx-auto">
+          {!data ? (
+            <div className="w-full h-full flex-grow flex justify-center items-center animate-pulse">
+              <div>Loading...</div>
+            </div>
+          ) : (
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+              {data?.slice(more.a, more.b).map((d, i) => {
+                const imageUrl = `${d.squareImgUrl}`;
+                return (
+                  <div
+                    key={i}
+                    className="flex flex-col text-black justify-between rounded-xl shadow-xl"
+                  >
+                    <div>
+                      <div className="h-[150px]">
+                        <img
+                          src={imageUrl}
+                          alt={d.name}
+                          className="object-cover w-full h-full"
+                        />
+                      </div>
+                      <div className="p-2 text-sm">
+                        <h1 className="font-semibold text-base">{d.name}</h1>
+                        <h1>{d.parentGeoName}</h1>
+                        <Rate
+                          value={d.averageRating}
+                          allowHalf
+                          disabled
+                          style={{ color: "#172554" }}
+                          className="py-1.5 text-[16px]"
+                        />
+                        <div className="flex justify-between text-xs uppercase">
+                          <div className="flex space-x-1">
+                            <h1 className="">
+                              {d.establishmentTypeAndCuisineTags[0]} •
+                            </h1>
+                            <h1 className=" text-green-600">{d.priceTag}</h1>
+                          </div>
+                          <h1>
+                            <span
+                              className={
+                                d.currentOpenStatusCategory === "OPEN"
+                                  ? "text-green-500"
+                                  : "text-red-500"
+                              }
+                            >
+                              {"• "}
+                            </span>
+                            {d.currentOpenStatusCategory
+                              ? d.currentOpenStatusCategory
+                              : "closed"}
                           </h1>
-                          <h1 className=" text-green-600">{d.priceTag}</h1>
                         </div>
-                        <h1>
-                          <span
-                            className={
-                              d.currentOpenStatusCategory === "OPEN"
-                                ? "text-green-500"
-                                : "text-red-500"
-                            }
-                          >
-                            {"• "}
-                          </span>
-                          {d.currentOpenStatusCategory
-                            ? d.currentOpenStatusCategory
-                            : "closed"}
-                        </h1>
                       </div>
                     </div>
+                    <Link
+                      to={`/detail/${d.id}`}
+                      className="w-full flex text-white text-xs justify-center py-3 bg-blue-950 hover:bg-blue-900 hover:font-semibold uppercase transition-all"
+                    >
+                      learn more
+                    </Link>
                   </div>
-                  <Link
-                    to={`/detail/${d.id}`}
-                    className="w-full flex text-white text-xs justify-center py-3 bg-blue-950 hover:bg-blue-900 hover:font-semibold uppercase transition-all"
-                  >
-                    learn more
-                  </Link>
-                </div>
-              );
-            })}
-          </div>
-        )}
-        {data ? (
-          <div className="w-full h-full flex flex-grow justify-center items-center py-9 font-semibold ">
-            {more.b !== data?.length && more.b < data?.length ? (
-              <button
-                onClick={
-                  more.b !== 24
-                    ? () => setMore({ ...more, b: more.b + 8 })
-                    : () => setMore({ ...more, b: more.b + 6 })
-                }
-                className="w-[400px] py-2 uppercase text-sm ring-1 active:scale-95 ring-black transition-all"
-              >
-                load more
-              </button>
-            ) : data?.length === 0 ? (
-              <div> Not found. </div>
-            ) : (
-              <div> You have seen all.</div>
-            )}
-          </div>
-        ) : null}
+                );
+              })}
+            </div>
+          )}
+          {data ? (
+            <div className="w-full h-full flex flex-grow justify-center items-center py-9 font-semibold ">
+              {more.b !== data?.length && more.b < data?.length ? (
+                <button
+                  onClick={
+                    more.b !== 24
+                      ? () => setMore({ ...more, b: more.b + 8 })
+                      : () => setMore({ ...more, b: more.b + 6 })
+                  }
+                  className="w-[400px] py-2 uppercase text-sm ring-1 active:scale-95 ring-black transition-all"
+                >
+                  load more
+                </button>
+              ) : data?.length === 0 ? (
+                <div> Not found. </div>
+              ) : (
+                <div> All items have been loaded.</div>
+              )}
+            </div>
+          ) : null}
+        </div>
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,136 +22,138 @@ export const Navbar = ({ sendCategory, sendChecked, sendPrice }) => {
   }, [price]);
 
   return (
-    <div className="flex justify-between py-3 px-[5%] md:px-[10%] text-sm text-center gap-x-1 border-y">
-      <div className="flex items-center gap-x-1 sm:gap-x-5 font-semibold">
-        <h1 className="hidden sm:flex">Filter by:</h1>
-        <div
-          className={`flex px-2 sm:px-3 py-1 border-b space-x-2 transition-all ${
-            checked && "text-white bg-blue-950 rounded-lg"
-          }`}
-        >
-          <input
-            type="radio"
-            checked={checked}
-            onChange={() => setChecked(true)}
-          />
-          <h1 className="sm:hidden">Open</h1>
-          <h1 className="hidden sm:flex">Open Now</h1>
+    <div className="py-3 px-[5%] md:px-[10%] border-y">
+      <div className="max-w-6xl mx-auto flex justify-between text-sm text-center gap-x-1">
+        <div className="flex items-center gap-x-1 sm:gap-x-5 font-semibold">
+          <h1 className="hidden sm:flex">Filter by:</h1>
+          <div
+            className={`flex px-2 sm:px-3 py-1 border-b space-x-2 transition-all ${
+              checked && "text-white bg-blue-950 rounded-lg"
+            }`}
+          >
+            <input
+              type="radio"
+              checked={checked}
+              onChange={() => setChecked(true)}
+            />
+            <h1 className="sm:hidden">Open</h1>
+            <h1 className="hidden sm:flex">Open Now</h1>
+          </div>
+          <div>
+            <button
+              onClick={() => setMenuPrice(!menuPrice)}
+              className={`flex items-end px-2 sm:px-3 py-1 border-b space-x-2 sm:space-x-3 transition-all ${
+                price !== "" && "text-white bg-blue-950 rounded-lg"
+              }`}
+            >
+              <span>Price</span>
+              <IoIosArrowDown
+                className={`${
+                  menuPrice ? `-rotate-180` : `-rotate-60`
+                } transition-all`}
+              />
+            </button>
+            {menuPrice && (
+              <div className="w-[69px] sm:w-[81px] absolute flex flex-col font-normal text-green-600 bg-white px-3 py-1 rounded-md transition-all">
+                <button
+                  onClick={() => {
+                    setPrice("$$ - $$$");
+                    setMenuPrice(false);
+                  }}
+                  className="text-left"
+                >
+                  $$
+                </button>
+                <button
+                  onClick={() => {
+                    setPrice("$$$$");
+                    setMenuPrice(false);
+                  }}
+                  className="text-left"
+                >
+                  $$$$
+                </button>
+              </div>
+            )}
+          </div>
+          <div>
+            <button
+              onClick={() => setMenuCategory(!menuCategory)}
+              className={`flex items-end px-2 sm:px-3 py-1 space-x-2 sm:space-x-3 border-b transition-all ${
+                category !== "" && "text-white bg-blue-950 rounded-lg"
+              }`}
+            >
+              <span>Categories</span>
+              <IoIosArrowDown
+                className={`${
+                  menuCategory ? `-rotate-180` : `-rotate-60`
+                } transition-all`}
+              />
+            </button>
+            {menuCategory && (
+              <div className="w-[105px] sm:w-[117px] absolute flex flex-col font-normal bg-white px-3 py-1 rounded-md">
+                <button
+                  onClick={() => {
+                    setCategory("Indian");
+                    setMenuCategory(false);
+                  }}
+                  className="text-left"
+                >
+                  Indian
+                </button>
+                <button
+                  onClick={() => {
+                    setCategory("Italian");
+                    setMenuCategory(false);
+                  }}
+                  className="text-left"
+                >
+                  Italian
+                </button>
+                <button
+                  onClick={() => {
+                    setCategory("Chinese");
+                    setMenuCategory(false);
+                  }}
+                  className="text-left"
+                >
+                  Chinese
+                </button>
+                <button
+                  onClick={() => {
+                    setCategory("Lebanese");
+                    setMenuCategory(false);
+                  }}
+                  className="text-left"
+                >
+                  Lebanese
+                </button>
+                <button
+                  onClick={() => {
+                    setCategory("Bar");
+                    setMenuCategory(false);
+                  }}
+                  className="text-left"
+                >
+                  Bar
+                </button>
+              </div>
+            )}
+          </div>
         </div>
         <div>
           <button
-            onClick={() => setMenuPrice(!menuPrice)}
-            className={`flex items-end px-2 sm:px-3 py-1 border-b space-x-2 sm:space-x-3 transition-all ${
-              price !== "" && "text-white bg-blue-950 rounded-lg"
-            }`}
+            onClick={() => {
+              setCategory("");
+              setChecked(false);
+              setPrice("");
+            }}
+            className=" uppercase px-3 sm:px-5 py-1 border active:scale-95 transition-all"
           >
-            <span>Price</span>
-            <IoIosArrowDown
-              className={`${
-                menuPrice ? `-rotate-180` : `-rotate-60`
-              } transition-all`}
-            />
+            <span className="hidden sm:flex transition-all">Clear all</span>
+            <span className="flex sm:hidden transition-all">Clear</span>
           </button>
-          {menuPrice && (
-            <div className="w-[69px] sm:w-[81px] absolute flex flex-col font-normal text-green-600 bg-white px-3 py-1 rounded-md transition-all">
-              <button
-                onClick={() => {
-                  setPrice("$$ - $$$");
-                  setMenuPrice(false);
-                }}
-                className="text-left"
-              >
-                $$
-              </button>
-              <button
-                onClick={() => {
-                  setPrice("$$$$");
-                  setMenuPrice(false);
-                }}
-                className="text-left"
-              >
-                $$$$
-              </button>
-            </div>
-          )}
         </div>
-        <div>
-          <button
-            onClick={() => setMenuCategory(!menuCategory)}
-            className={`flex items-end px-2 sm:px-3 py-1 space-x-2 sm:space-x-3 border-b transition-all ${
-              category !== "" && "text-white bg-blue-950 rounded-lg"
-            }`}
-          >
-            <span>Categories</span>
-            <IoIosArrowDown
-              className={`${
-                menuCategory ? `-rotate-180` : `-rotate-60`
-              } transition-all`}
-            />
-          </button>
-          {menuCategory && (
-            <div className="w-[105px] sm:w-[117px] absolute flex flex-col font-normal bg-white px-3 py-1 rounded-md">
-              <button
-                onClick={() => {
-                  setCategory("Indian");
-                  setMenuCategory(false);
-                }}
-                className="text-left"
-              >
-                Indian
-              </button>
-              <button
-                onClick={() => {
-                  setCategory("Italian");
-                  setMenuCategory(false);
-                }}
-                className="text-left"
-              >
-                Italian
-              </button>
-              <button
-                onClick={() => {
-                  setCategory("Chinese");
-                  setMenuCategory(false);
-                }}
-                className="text-left"
-              >
-                Chinese
-              </button>
-              <button
-                onClick={() => {
-                  setCategory("Lebanese");
-                  setMenuCategory(false);
-                }}
-                className="text-left"
-              >
-                Lebanese
-              </button>
-              <button
-                onClick={() => {
-                  setCategory("Bar");
-                  setMenuCategory(false);
-                }}
-                className="text-left"
-              >
-                Bar
-              </button>
-            </div>
-          )}
-        </div>
-      </div>
-      <div>
-        <button
-          onClick={() => {
-            setCategory("");
-            setChecked(false);
-            setPrice("");
-          }}
-          className=" uppercase px-3 sm:px-5 py-1 border active:scale-95 transition-all"
-        >
-          <span className="hidden sm:flex transition-all">Clear all</span>
-          <span className="flex sm:hidden transition-all">Clear</span>
-        </button>
       </div>
     </div>
   );
